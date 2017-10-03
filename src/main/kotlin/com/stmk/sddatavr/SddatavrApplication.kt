f9d641e26629d30b9a3849d755d424bb761dc434
krishnakandula <krishna.c.kandula@gmail.com>
2017-10-03 17:55:30 -0500

    Add initial project

---
+++ src/main/kotlin/com/stmk/sddatavr/SddatavrApplication.kt
@@ -0,0 +1,11 @@
+package com.stmk.sddatavr
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SddatavrApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SddatavrApplication::class.java, *args)
+}
