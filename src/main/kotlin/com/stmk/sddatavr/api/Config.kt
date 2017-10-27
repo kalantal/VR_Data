b359c054e7ba00c7de5404915c970d517254326a
krishnakandula <krishna.c.kandula@gmail.com>
2017-10-27 08:17:43 -0500

    Add support for nested queries

--- src/main/kotlin/com/stmk/sddatavr/api/Config.kt
+++
@@ -1,9 +0,0 @@
-package com.stmk.sddatavr.api
-
-/**
- * Created by Krishna Chaitanya Kandula on 10/3/17.
- */
-
-class Config {
-
-}
--- src/main/kotlin/com/stmk/sddatavr/search/models/Query.kt
+++ src/main/kotlin/com/stmk/sddatavr/search/models/Query.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by Krishna Chaitanya Kandula on 10/23/17.
  */
-data class Query(@SerializedName("field") val field: String,
-                 @SerializedName("value") val value: Any,
-                 @SerializedName("occurance") val occurance: Occur)
+data class Query(@SerializedName("field") val field: String?,
+                 @SerializedName("value") val value: Any?,
+                 @SerializedName("occurance") val occurance: Occur,
+                 @SerializedName("nested_query") val nestedQueries: List<Query>?)
